194269c4d22949feab13e385e66e889b3e99deda
li_shugan <li_shugan@126.com>
2016-09-11 23:19:02 +0800

    [Noraml] improve FontPattern

--- server/FontPattern.cpp
+++ server/FontPattern.cpp
@@ -1,8 +1,18 @@
+#include <fstream>
 #include "FontPattern.h"
 
 std::shared_ptr<FontPattern> FontPattern::Load(const std::string& filePath)
 {
-    
+    if(filePath.length() < 4 || filePath.substr(filePath.length() - 3) != ".vc")
+    {
+        return nullptr;
+    }
+
+
+    std::string fileName = filePath.substr(filePath.find_last_of('/') + 1);
+    std::string stem = fileName.substr(0, fileName.length() - 3);
+
+    std::ifstream fontFile(filePath, std::ios_base::binary | std::ios_base::in);
 }
 
 def __init__(self, name, pattern):
--- server/FontPattern.h
+++ server/FontPattern.h
@@ -1,12 +1,34 @@
 #ifndef _FONT_PATTERN_H
 #define _FONT_PATTERN_H
 #include <memory>
+struct FontSampleCoor
+{
+    int32_t w,h;
+}
+
 class FontPattern
 {
 public:
     static std::shared_ptr<FontPattern> Load(const std::string& filePath);
 
+    int32_t GetMinW() const;
+    int32_t GetMaxW() const;
+
+    int32_t GetMinH() const;
+    int32_t GetMaxH() const;
+
+    int32_t GetWidth() const;
+    int32_t GetHeight() const;
+
 private:
-    std::vector<> ;
+    void SetCenter(FontSampleCoor& center);
+
+
+    std::string mName;
+    int32_t mMinW;
+    int32_t mMaxW;
+    int32_t mMinH;
+    int32_t mMaxH;
+    std::vector<FontSampleCoor> mSamples;
 };
 #endif
