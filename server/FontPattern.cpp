a28463f9950ef0916072c797197ed76317391313
li_shugan <li_shugan@126.com>
2016-09-13 22:32:06 +0800

    [Normal] finish FontPattern

--- server/FontPattern.cpp
+++ server/FontPattern.cpp
@@ -1,5 +1,58 @@
 #include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
 #include "FontPattern.h"
+
+bool operator < (const FontSample& s1, const FontSample& s2)
+{
+    return s1.h == s2.h ? s1.w < s2.w : ((s1.h - s2.h) < 0);
+}
+
+FontPattern::FontPattern(const std::string& name)
+    :mName(name)
+    ,mMinW(0)
+    ,mMaxW(0)
+    ,mMinH(0)
+    ,mMaxH(0)
+    ,mWidth(0)
+    ,mHeight(0)
+{
+}
+
+void FontPattern::AddSample(FontSample& sample)
+{
+    if(!mSamples.insert(sample).second)
+    {
+        return;
+    }
+
+    if(mMinW > sample.w)
+    {
+        mMinW = sample.w;
+    }
+
+    if(mMaxW <= sample.w)
+    {
+        mMaxW = sample.w + 1;
+    }
+
+    if(mMinH > sample.h)
+    {
+        mMinH = sample.h;
+    }
+
+    if(mMaxH <= sample.h)
+    {
+        mMaxH = sample.h + 1;
+    }
+}
+
+void FontPattern::ComputeSize( void )
+{
+    mWidth = GetMaxW() - GetMinW();
+    mHeight = GetMaxH() - GetMinW();
+}
 
 std::shared_ptr<FontPattern> FontPattern::Load(const std::string& filePath)
 {
@@ -13,17 +66,67 @@ std::shared_ptr<FontPattern> FontPattern::Load(const std::string& filePath)
     std::string stem = fileName.substr(0, fileName.length() - 3);
 
     std::ifstream fontFile(filePath, std::ios_base::binary | std::ios_base::in);
+    if(!fontFile.is_open())
+    {
+        std::cout << "open font file " << filePath << " failed\n";
+        return nullptr;
+    }
+
+    auto pFontPattern = std::make_shared<FontPattern>(fileName);
+    std::string line;
+    std::vector<int> cols;
+
+    int32_t nWidth = -1;
+    int32_t nH = 0;
+    while(std::getline(fontFile, line))
+    {
+        Split(line, cols);
+        if(-1 == nWidth)
+        {
+            nWidth = cols.size();
+        }
+        else if(nWidth != cols.size())
+        {
+            std::cout << "font file " << filePath << " maybe damaged: line " << (nH + 1) << " should be " << nWidth << '\n' ;
+            return nullptr;
+        }
+
+        int32_t nW = 0;
+        for(auto c : cols)
+        {
+            if(0 != c)
+            {
+                pFontPattern->AddSample({nW++, nH});
+            }
+        }
+        nH++;
+    }
+
+    pFontPattern->SetCenter({nWidth / 2, nH / 2});
+
+    return pFontPattern;
 }
 
-def __init__(self, name, pattern):
-    self.name = name
-    self.height, self.width = pattern.shape
-    elements = []
-    top, left = self.height / 2, self.width / 2
-    for h in xrange(0, pattern.shape[0]):
-        for w in xrange(0, pattern.shape[1]):
-            if 0 != pattern[h, w]:
-                ww, hh = w - left, h - top
-                elements.append((ww, hh))
+void FontPattern::Split(const std::string& line, std::vector<int>& outValues)
+{
+    outValues.clear();
+    std::vector<std::string> tokens{std::istringstream(line), std::istringstream};
+    std::transform(tokens.begin(), tokens.end(), outValues.begin(), [](const std::string& v) { return std::stoi(v); });
+}
 
-    self.elements = elements
+void FontPattern::SetCenter(FontSample& center)
+{
+    for(auto& cSample : mSamples)
+    {
+        auto& sample = const_cast<FontSample&>(cSample);
+        sample.w -= center.w;
+        sample.h -= center.h;
+    }
+
+    mMinW -= center.w;
+    mMaxW -= center.w;
+
+    mMinH -= center.h;
+    mMaxH -= center.h;
+    ComputeSize();
+}
--- server/FontPattern.h
+++ server/FontPattern.h
@@ -1,9 +1,14 @@
 #ifndef _FONT_PATTERN_H
 #define _FONT_PATTERN_H
 #include <memory>
-struct FontSampleCoor
+#include <vector>
+#include <set>
+
+struct FontSample
 {
     int32_t w,h;
+
+    friend bool operator < (const FontSample& s1, const FontSample& s2);
 }
 
 class FontPattern
@@ -11,24 +16,30 @@ class FontPattern
 public:
     static std::shared_ptr<FontPattern> Load(const std::string& filePath);
 
-    int32_t GetMinW() const;
-    int32_t GetMaxW() const;
+    FontPattern(const std::string& name);
 
-    int32_t GetMinH() const;
-    int32_t GetMaxH() const;
+    int32_t GetMinW() const { return mMinW; }
+    int32_t GetMaxW() const { return mMaxW; }
 
-    int32_t GetWidth() const;
-    int32_t GetHeight() const;
+    int32_t GetMinH() const { return mMinH; }
+    int32_t GetMaxH() const { return mMaxH; }
+
+    int32_t GetWidth() const { return GetMaxW() - GetMinW() + 1; }
+    int32_t GetHeight() const { return GetMaxH() - GetMinW() + 1; }
 
 private:
-    void SetCenter(FontSampleCoor& center);
-
+    void AddSample(FontSample& sample);
+    void ComputeSize( void );
+    void SetCenter(FontSample& center);
+    static void Split(const std::string& line, std::vector<int>& outValues);
 
     std::string mName;
     int32_t mMinW;
     int32_t mMaxW;
     int32_t mMinH;
     int32_t mMaxH;
-    std::vector<FontSampleCoor> mSamples;
+    int32_t mWidth;
+    int32_t mHeight;
+    std::set<FontSample> mSamples;
 };
 #endif
