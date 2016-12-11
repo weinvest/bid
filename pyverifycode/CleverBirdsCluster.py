e6db1b1ff3760b08788e26b136efb8969cf7cd8f
li_shugan <li_shugan@126.com>
2016-12-11 22:16:34 +0800

    [Normal] improve

--- pyverifycode/CleverBirdsCluster.py
+++ pyverifycode/CleverBirdsCluster.py
@@ -1,30 +1,43 @@
 import math
 import numpy as np
+import pandas as pd
+#params:
+#    dist: a pandas.DataFrame object with v1,v2 as it's index and dist as it's value
+#return:
+#    vertexProperties: a pandas.DataFrame object with index=union(v1,v2)
 def createVertexProperties(dist):
-    pass
+    vertexIds = np.union1d(dist.index.get_level_value(0), dist.index.get_level_value(1))
+    numOfVertics = len(vertexIds)
+    maxDelta = dist.dist.max()
+    return pd.DataFrame(index=vertexIds
+                        , data={'rtho': np.zeros((numOfVertics, 1)),
+                                'delta': [maxDelta] * numOfVertics,
+                                'gamma': [0] * numOfVertics,
+                                'cluster': [-1] * numOfVertics})
 
 def computeDistThreshold(vertexProperties, dist, avgPercentOfNeighbors):
     numOfVertices = len(vertexProperties)
     position = int(numOfVertices * numOfVertices * 0.5 * avgPercentOfNeighbors)
-    threshold = sorted(dist[:,3])[position]
+    threshold = sorted(dist.dist)[position]
 
     return threshold
 
-
 def computeRtho(vertexProperties, dist, distThreshold):
-    for v1Id, v2Id, dist in dist:
-        v1 = vertexProperties[v1Id]
-        v2 = vertexProperties[v2Id]
-
-        deltaRtho = math.exp(-dist/distThreshold)
-        v1.rtho += deltaRtho
-        v2.rtho += deltaRtho
+    deltaRtho = np.exp(-dist.dist/distThreshold)
+    vertexProperties.rtho += deltaRtho.reset_index(level='v2', drop=True)
+    vertexProperties.rtho += deltaRtho.reset_index(level='v1', drop=True)
 
 def computeDelta(vertexProperties, dist):
-    def vertexComparae(v1, v2):
-        return v1.rtho - v2.rtho
+    numOfVertices = len(vertexProperties)
+    vertexProperties.sort_values('rtho')
+    vertexProperties.map()
 
-    sortedVertices = sorted(vertexProperties, vertexComparae)
-    for v in sortedVertices[1:]:
-        pass
+def computeGamma(vertexProperties, plot = False):
+    vertexProperties.gamma = vertexProperties.rtho * vertexProperties.delta
+
+    if plot:
+        vertexProperties.plot(x = 'rho', y = 'delta')
+        vertexProperties.plot(x = '')
+
+def
 
