5fd7fd89478e0b3cba8a88703b0e044b13f59c60
shugan.li <shugan.li@dfctec.com>
2016-12-13 09:52:17 +0800

    [Improve]

--- pyverifycode/CleverBirdsCluster.py
+++ pyverifycode/CleverBirdsCluster.py
@@ -6,13 +6,13 @@ import pandas as pd
 #return:
 #    vertexProperties: a pandas.DataFrame object with index=union(v1,v2)
 def createVertexProperties(dist):
-    vertexIds = np.union1d(dist.index.get_level_value(0), dist.index.get_level_value(1))
+    vertexIds = np.union1d(dist.index.get_level_values(0), dist.index.get_level_values(1))
     numOfVertics = len(vertexIds)
     maxDelta = dist.dist.max()
     return pd.DataFrame(index=vertexIds
-                        , data={'rtho': np.zeros((numOfVertics, 1)),
+                        , data={'rtho': [0] * numOfVertics,
                                 'delta': [maxDelta] * numOfVertics,
-                                'nearestVertexId' : [-1] * numOfVertics,
+                                'nearestVertexId': [-1] * numOfVertics,
                                 'gamma': [0] * numOfVertics,
                                 'cluster': [-1] * numOfVertics,
                                 'halo': [0] * numOfVertics})
@@ -20,14 +20,15 @@ def createVertexProperties(dist):
 def computeDistThreshold(vertexProperties, dist, avgPercentOfNeighbors = 0.02):
     numOfVertices = len(vertexProperties)
     position = int(numOfVertices * numOfVertices * 0.5 * avgPercentOfNeighbors)
-    threshold = sorted(dist.dist)[position]
+    sortedDist = sorted(dist.dist)
+    threshold = sortedDist[int(len(sortedDist) * 0.1)]
 
     return threshold
 
 def computeRtho(vertexProperties, dist, distThreshold):
     deltaRtho = np.exp(-dist.dist/distThreshold)
-    vertexProperties.rtho += deltaRtho.reset_index(level='v2', drop=True)
-    vertexProperties.rtho += deltaRtho.reset_index(level='v1', drop=True)
+    vertexProperties.rtho += deltaRtho.groupby(level='v1').sum()
+    vertexProperties.rtho += deltaRtho.groupby(level='v2').sum()
 
 def computeDelta(vertexProperties, dist):
     numOfVertices = len(vertexProperties)
@@ -36,7 +37,10 @@ def computeDelta(vertexProperties, dist):
     itRows = vertexProperties.iterrows()
     nbVertexIds = [itRows.next()[0]]
     for vertexId, pros in itRows:
-        nearest = dist.loc[dist.v1.isin(nbVertexIds) | dist.v2.isin(nbVertexIds)].dist.idxmin()
+        print dist.loc[vertexId,:]
+
+        nearest = dist.loc[dist.index.get_level_values(0).isin(nbVertexIds) \
+                           | dist.index.get_level_values(1).isin(nbVertexIds)].dist.idxmin()
         pros.nearestVertexId = nearest[0]
         pros.delta = nearest[1].dist
         nbVertexIds.append(vertexId)
@@ -86,8 +90,8 @@ import Image
 import numpy as np
 import pandas as pd
 import color
-if __name__ == '__main__':
 
+def testImage():
     def getVertexId(bmp, p):
         return bmp.width * p[1] + p[0]
 
@@ -112,8 +116,47 @@ if __name__ == '__main__':
             dicDist.extend([color.dist(c, bmp.getpixel(v)) for v in neighbors])
 
     dist = pd.DataFrame({'v1': fromVertex, 'v2': toVertex, 'dist': dicDist})
-    dist.set_index(['v1', 'v2'])
+    dist.set_index(['v1', 'v2'], inplace=True)
+    #print dist
 
+    vertexProperties = createVertexProperties(dist)
+    distThreshold = computeDistThreshold(vertexProperties, dist, 0.00002)
+    computeRtho(vertexProperties, dist, distThreshold)
+    computeDelta(vertexProperties, dist)
+    computeGamma(vertexProperties, dist)
+
+from scipy import spatial
+if __name__ == '__main__':
+    fileName = sys.argv[1]
+    location = []
+    label = []
+    for line in open(fileName, "r"):
+        items = line.strip("\n").split(",")
+        label.append(int(items.pop()))
+        tmp = []
+        for item in items:
+            tmp.append(float(item))
+        location.append(tmp)
+    location = np.array(location)
+    label = np.array(label)
+    length = len(location)
+
+    print spatial.distance.pdist(location)
+    #Caculate distance
+    v1 = []
+    v2 = []
+    dist = []
+    begin = 0
+    while begin < length-1:
+        end = begin + 1
+        while end < length:
+            dd = np.linalg.norm(location[begin]-location[end])
+            v1.append(begin)
+            v2.append(end)
+            dist.append(dd)
+
+    dist = pd.DataFrame({'v1': v1, 'v2': v2, 'dist': dist})
+    dist.set_index(['v1', 'v2'], inplace=True)
     vertexProperties = createVertexProperties(dist)
     distThreshold = computeDistThreshold(vertexProperties, dist)
     computeRtho(vertexProperties, dist, distThreshold)
